de2c56aacc670def122353acf3684dacbfcadf8e
Temuulen-Ectrolad <temuulen8811@gmail.com>
2023-04-13 19:24:58 +0800

    Temuulen -> User Authentication UI

--- BingParking/src/screens/Home.tsx
+++ BingParking/src/screens/Home.tsx
@@ -1,9 +1,9 @@
-import { Button, FlatList, Image, Text, View } from "react-native"
+import { Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native"
 import { t } from "../utils/style"
 import { useState } from "react";
 import ImagePicker, { ImageOrVideo } from "react-native-image-crop-picker";
 
-export const HomeScreen = () => {
+export const HomeScreen = ({navigation}: any) => {
 
   const [images, setImages] = useState<ImageOrVideo[]>([]);
 
@@ -22,20 +22,21 @@ export const HomeScreen = () => {
       });
   };
 
-  return (
-    <View style={t`flex flex-col w-full justify-center items-center`}>
-      <Text style={t`text-green-500`}>HomeScreen</Text>
-      <Text style={t`text-green-500`}>Good Luck BingParking Devs! ^^</Text>
-      <View style={t`flex flex-col w-full`}>
-        <Text>Testing Multiple photo uploads.</Text>
-        <Button title="Select Images" onPress={selectImages} />
-        <FlatList
-          data={images}
-          renderItem={({ item }) => (
-            <Image source={{ uri: item.path }} style={{ width: 200, height: 200, margin: 10 }} />)}
-          keyExtractor={(item) => item.path}
-          horizontal={true} />
-      </View>
-    </View>
-  )
+    return (
+        <View style={t`flex flex-col w-full justify-center items-center`}>
+            <Text style={t`text-green-500`}>HomeScreen</Text>
+            <Text style={t`text-green-500`}>Good Luck BingParking Devs! ^^</Text>
+            <View style={t`flex flex-col w-full`}>
+              <Text>Testing Multiple photo uploads.</Text>
+              <Button title="Select Images" onPress={selectImages} />
+              <FlatList
+               data={images}
+               renderItem={({ item }) => (
+               <Image source={{ uri: item.path }} style={{ width: 200, height: 200, margin: 10 }}/>)}
+               keyExtractor={(item) => item.path}
+               horizontal={true}/>
+            </View>
+            <TouchableOpacity  onPress={() => navigation?.navigate('Starter')}><Text>User-Authentication UI</Text></TouchableOpacity>
+        </View>
+    )
 }; 
