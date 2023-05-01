4043981252fa6a556868abc7c4bfad92e9c2c49e
UnknownOne76 <ganboldgantor14@gmail.com>
2023-05-01 22:08:53 +0800

    Back-End Set-Up -> UKN76

--- bing-back-end/src/users/createUser.ts
+++ bing-back-end/src/users/createUser.ts
@@ -1,1 +1,17 @@
-//tbc...
+import { APIGatewayProxyEvent , APIGatewayProxyResult } from "aws-lambda";
+import { dynamoClient, env, pv, s3Client } from "../utils";
+import moment = require("moment");
+// env;
+export const createUsr = async(e: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const { fname , name , date , mail , phone , pic , gender } = JSON.parse(e?.body as string);
+    try {
+        // console.log(date.includes('Today') ? date : moment(new Date(date)).format('LL')); 
+        // const date = moment(new Date('2023-4-19')).calendar(); 
+    } catch(err) {
+        console.log(err);
+    }
+    return {
+        statusCode: 200,
+        body: 'Transfer'
+    };  
+};  
