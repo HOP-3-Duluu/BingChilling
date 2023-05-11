b9304157d371794528e92a22387f56e7c2b7924e
Temuulen-Ectrolad <temuulen8811@gmail.com>
2023-05-11 20:08:35 +0800

    Temuulen has coded front-end of location add

--- bing-back-end/src/users/createUser.ts
+++ bing-back-end/src/users/createUser.ts
@@ -1,5 +1,4 @@
 import { APIGatewayProxyEvent , APIGatewayProxyResult } from "aws-lambda";
-<<<<<<< Updated upstream
 import { dynamoClient, env, headers, pv, s3Client } from "../utils";
 import { v4 as uuidv4 } from 'uuid';
 import { marshall } from "@aws-sdk/util-dynamodb";
@@ -33,42 +32,6 @@ export const createUsr = async(e: APIGatewayProxyEvent): Promise<APIGatewayProxy
                 console.log(`Created in dynamo Table`);
             }).catch(err => console.log(err));
         });
-=======
-import { dynamoClient, env, pv, s3Client, uid } from "../utils";
-import { marshall } from '@aws-sdk/util-dynamodb';
-// env;
-// console.log(date.includes('Today') ? date : moment(new Date(date)).format('LL')); 
-// const date = moment(new Date('2023-4-19')).calendar(); 
-export const createUsr = async(e: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const { fname , name , mail , phone , pic , gender , role } = JSON.parse(e?.body as string);
-    try {
-        const params = {
-        Bucket: 'fs-bucket01', 
-        Key: `users/${uid}.jpeg`, 
-        Body: Buffer.from(pic.replace(/^data:image\/\w+;base64,/, ""), "base64"),
-        ContentType: "image/jpeg", 
-        ACL: 'public-read' 
-        };
-        
-        await s3Client.putObject(params).then(async() => {
-            const params1 = {
-                TableName: 'users',
-                Item: marshall({
-                 fullname : fname,
-                 name: name,
-                 email: mail,
-                 phone: phone,
-                 photo: `https://fs-bucket01.s3.ap-northeast-2.amazonaws.com/users/${uid}.jpeg`,
-                 gender: gender,
-                 role: role
-                })
-            };
-            await dynamoClient.putItem(params1).then(() => {
-                console.log(`Sent.`); 
-            });        
-        });
-
->>>>>>> Stashed changes
     } catch(err) {
         console.log(err);
     }
